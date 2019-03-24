b79ee1d1c4f64a1bee01e3ed8733f7a0c8a66e7e
Tommy <48854815+tommy-lai@users.noreply.github.com>
2019-03-24 23:04:14 +1100

    Add png package required

--- Animate_Shares.R
+++ Animate_Shares.R
@@ -1,5 +1,5 @@
 # Check required Pacakages for installation
-list.of.packages <- c("ggplot2", "gganimate", "gifski", "quantmod", "dplyr", "magick")
+list.of.packages <- c("ggplot2", "gganimate", "gifski", "quantmod", "dplyr", "magick", "png")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
